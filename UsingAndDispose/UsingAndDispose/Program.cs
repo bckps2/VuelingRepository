bb8521ae11028014f6114b03b8d2d468246bad58
Becquer <bckfl10@gmail.com>
2019-02-10 23:14:26 +0100

    Ejercicio Finalizado

---
+++ UsingAndDispose/UsingAndDispose/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingAndDispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ejemplo de utilizacion USING
+            Char[] buffer = new Char[50];
+            using (StreamReader reader = new StreamReader("File1.txt")) {
+                 int charRead = 0;
+                while (reader.Peek() != -1) {
+                charRead = reader.Read(buffer, 0, buffer.Length);
+           
+                }
+
+            }
+
+            //ejemplo de utilizaion de Dispose.
+            StreamReader sReader = null;
+              try {
+                 sReader = new StreamReader("file1.txt");
+                 String text= sReader.ReadToEnd(); 
+              }      
+              finally {
+                 if (sReader != null) sReader.Dispose();     
+              }
+   }
+        }
+    }
+}
+    
