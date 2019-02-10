cbbb53bff0f5b077b746883a4d50d846e5eda7e6
Becquer <bckfl10@gmail.com>
2019-02-10 13:23:46 +0100

    no message

---
+++ TestExtensores/TestExtensores/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Person
+/// </summary>
+public class Person
+{
+    public String Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ TestExtensores/TestExtensores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExtensores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+            persona1.Name= "";
+            System.Diagnostics.Debug.Write(persona1.HasName()+"\n");
+        }
+    }
+}
