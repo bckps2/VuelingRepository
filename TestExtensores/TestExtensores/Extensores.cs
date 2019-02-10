517fc29f86ee9d2257d480a992afb4fb38bfa412
Becquer <bckfl10@gmail.com>
2019-02-10 18:38:08 +0100

    Confirmacions

---
+++ TestExtensores/TestExtensores/Extensores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Extensores
+/// </summary>
+public class Extensores
+
+{
+    
+    public static Boolean HasName(this Persona person)
+    {
+        if (string.IsNullOrEmpty(person.Name) || string.IsNullOrWhiteSpace(person.Name))
+        {
+            return false;
+        }
+        else { return true; }
+
+    }
+}
