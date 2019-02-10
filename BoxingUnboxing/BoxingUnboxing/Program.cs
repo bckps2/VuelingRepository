467f8fbb48b00dea75fbde1f76f6c2221048ac8c
Becquer <bckfl10@gmail.com>
2019-02-10 12:25:35 +0100

    Se ha completado el ejercicio

---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este codigo representa un boxing Quiere decir que pasamos 
+            //una variable tipo int a un objeto
+            int Number = 2;
+            Object O = Number;
+            System.Diagnostics.Debug.Write(O);
+
+            //Este codigo realiza el unboxing quiere decir que pasamos un objeto a variable tipo int
+            O = 456;
+            Number = (int)O;
+            System.Diagnostics.Debug.Write(Number);
+        }
+    }
+}
